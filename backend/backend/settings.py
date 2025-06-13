fc342c64fff7b67555b1ca147d12d7f9d7ab8f4a
golebiewskist <166710028+golebiewskist@users.noreply.github.com>
2025-06-13 14:57:06 +0200

    - Custom pagination allowing change of page size (default 5)

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -126,6 +127,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "accounts.CustomUser"
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'backend.pagination.CustomPageNumberPagination',
+
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
